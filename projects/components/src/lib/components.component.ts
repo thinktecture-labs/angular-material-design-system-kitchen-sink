1217ac21f4a7cee575f3a923d9d5bec9d637ea6f
Konstantin Denerz <konstantin.denerz@gmail.com>
2022-06-17 14:27:27 +0200

    setup app and component library

---
+++ projects/components/src/lib/components.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'labs-components',
+  template: `
+    <p>
+      components works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ComponentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
