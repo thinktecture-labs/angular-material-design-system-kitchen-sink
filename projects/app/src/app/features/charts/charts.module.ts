bd0e1ff25757d36fb2b3546ffbf455f3968242da
Sascha Lehmann <sascha.lehmann92@gmail.com>
2022-09-07 12:53:49 +0200

    feat(app): added charts module and doughnut component

---
+++ projects/app/src/app/features/charts/charts.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DoughnutComponent } from './doughnut/doughnut.component';
+
+
+
+@NgModule({
+  declarations: [
+    DoughnutComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ChartsModule { }
