36257355dd10d3dec6504560e033deaed1d8f5c8
Sascha Lehmann <sascha.lehmann92@gmail.com>
2022-09-07 10:51:59 +0200

    feat(app): added on additional talk to mock data

--- projects/app/src/app/features/talks/talk.data.ts
+++ projects/app/src/app/features/talks/talk.data.ts
@@ -31,5 +31,15 @@ export const TALK_MOCK_DATA: Talk[] = [
     bannerImageSrc: "https://privacy-proxy-server.usercentrics.eu/video/youtube/cub2euPSNb8-poster-image",
     tags: ["angular", "ionic"]
   },
+  {
+    title: "Schneller ans Ziel: Web-Apps mit Ionic",
+    conference: "Developer Week '22",
+    startDate: '04.07.2022',
+    endDate: '08.07.2022',
+    location: "Nürnberg",
+    country: "DE",
+    bannerImageSrc: "https://privacy-proxy-server.usercentrics.eu/video/youtube/cub2euPSNb8-poster-image",
+    tags: ["angular", "ionic"]
+  },
 
 ]
