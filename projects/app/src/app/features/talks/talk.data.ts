0ffbb07dc2d749d94611c28f6f0140c5d416406d
Sascha Lehmann <sascha.lehmann92@gmail.com>
2022-10-06 10:33:27 +0200

    refactor(app): added tags to card representation

--- projects/app/src/app/features/talks/talk.data.ts
+++ projects/app/src/app/features/talks/talk.data.ts
@@ -8,7 +8,7 @@ export const TALK_MOCK_DATA: Talk[] = [
     endDate: '08.07.2022',
     location: "Nürnberg",
     country: "DE",
-    bannerImageSrc: "https://privacy-proxy-server.usercentrics.eu/video/youtube/cub2euPSNb8-poster-image",
+    bannerImageSrc: "https://i.ytimg.com/vi/8F841ao34fU/maxresdefault.jpg",
     tags: ["angular", "ionic"]
   },
   {
@@ -18,7 +18,7 @@ export const TALK_MOCK_DATA: Talk[] = [
     endDate: '08.07.2022',
     location: "Nürnberg",
     country: "DE",
-    bannerImageSrc: "https://privacy-proxy-server.usercentrics.eu/video/youtube/cub2euPSNb8-poster-image",
+    bannerImageSrc: "https://basta.net/wp-content/uploads/2022/07/BASTA_MZ22_OG_GER_1200x630_64454_v2.png",
     tags: ["angular", "ionic"]
   },
   {
@@ -28,18 +28,7 @@ export const TALK_MOCK_DATA: Talk[] = [
     endDate: '08.07.2022',
     location: "Nürnberg",
     country: "DE",
-    bannerImageSrc: "https://privacy-proxy-server.usercentrics.eu/video/youtube/cub2euPSNb8-poster-image",
+    bannerImageSrc: "https://javascript-days.de/wp-content/uploads/2022/01/JSD_MUC21_Kombibanner_FB_1200x600_63404_v1.jpg",
     tags: ["angular", "ionic"]
   },
-  {
-    title: "Schneller ans Ziel: Web-Apps mit Ionic",
-    conference: "Developer Week '22",
-    startDate: '04.07.2022',
-    endDate: '08.07.2022',
-    location: "Nürnberg",
-    country: "DE",
-    bannerImageSrc: "https://privacy-proxy-server.usercentrics.eu/video/youtube/cub2euPSNb8-poster-image",
-    tags: ["angular", "ionic"]
-  },
-
 ]
--- projects/app/src/app/features/talks/talks.module.ts
+++ projects/app/src/app/features/talks/talks.module.ts
@@ -6,6 +6,7 @@ import {TalksComponent} from './talks/talks.component';
 import {CardModule} from "../../../../../components/src/lib/card/card.module";
 import {ButtonModule} from "../../../../../components/src/lib/button/button.module";
 import {TalkListItemComponent} from "./talk-list-item/talk-list-item.component";
+import {ChipModule} from "../../../../../components/src/lib/chip/chip.module";
 
 
 @NgModule({
@@ -20,7 +21,8 @@ import {TalkListItemComponent} from "./talk-list-item/talk-list-item.component";
     CommonModule,
     TalksRoutingModule,
     CardModule,
-    ButtonModule
+    ButtonModule,
+    ChipModule
   ]
 })
 export class TalksModule {
