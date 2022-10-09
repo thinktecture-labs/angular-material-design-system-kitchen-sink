13f7223b0fd563cc02d76dd22b0e70ba6b13dbcf
Konstantin Denerz <konstantin.denerz@gmail.com>
2022-10-09 15:23:15 +0200

    refactor(components): add emphasis primary

--- projects/components/src/lib/styles/themes/mock/color-mapping.ts
+++ projects/components/src/lib/styles/themes/mock/color-mapping.ts
@@ -2,6 +2,7 @@ export interface Color {
   key: string,
   description?: string,
   value?: string,
+  background?: string;
 }
 
 export interface ColorGroup {
@@ -28,13 +29,25 @@ export const GROUPS: ColorGroup[] = [
     ]
   },
   {
-    key: 'emphasis',
+    key: 'emphasis (surface)',
     colors: [
-      {key: '--md-sys-color-emphasis-1'},
-      {key: '--md-sys-color-emphasis-2'},
-      {key: '--md-sys-color-emphasis-3'},
-      {key: '--md-sys-color-emphasis-4'},
-      {key: '--md-sys-color-emphasis-5'},
+      {key: '--md-sys-color-surface-emphasis-0'},
+      {key: '--md-sys-color-surface-emphasis-1'},
+      {key: '--md-sys-color-surface-emphasis-2'},
+      {key: '--md-sys-color-surface-emphasis-3'},
+      {key: '--md-sys-color-surface-emphasis-4'},
+      {key: '--md-sys-color-surface-emphasis-5'},
+    ]
+  },
+  {
+    key: 'emphasis (primary)',
+    colors: [
+      {key: '--md-sys-color-primary-emphasis-0', background: 'var(--md-sys-color-primary'},
+      {key: '--md-sys-color-primary-emphasis-1', background: 'var(--md-sys-color-primary'},
+      {key: '--md-sys-color-primary-emphasis-2', background: 'var(--md-sys-color-primary'},
+      {key: '--md-sys-color-primary-emphasis-3', background: 'var(--md-sys-color-primary'},
+      {key: '--md-sys-color-primary-emphasis-4', background: 'var(--md-sys-color-primary'},
+      {key: '--md-sys-color-primary-emphasis-5', background: 'var(--md-sys-color-primary'},
     ]
   },
   {
