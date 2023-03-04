35a5bbea0ebdd11913a2764a79727b0e550c5e69
SaschaLeh <sascha.lehmann@thinktecture.com>
2023-03-04 18:00:19 +0100

    feat: home - stagger

--- projects/animation-showcase/src/app/components/home/home.component.ts
+++ projects/animation-showcase/src/app/components/home/home.component.ts
@@ -3,11 +3,39 @@ import {TALK_MOCK_DATA} from './talk.data';
 import {BehaviorSubject, combineLatest, map, of} from 'rxjs';
 import {Talk} from './talk.model';
 import {FormControl} from '@angular/forms';
+import {animate, query, stagger, style, transition, trigger} from '@angular/animations';
+
+const fadeInAnimation = [transition(':enter', [
+  style({opacity: 0, transform: 'translateY(-10px)'}),
+  animate('0.2s ease-out', style({opacity: 1, transform: 'tanslateY(o)'})),
+]),
+  transition(':leave', [
+    style({opacity: 1, transform: 'translateY(0)'}),
+    animate('0.1s ease-in', style({opacity: 0, transform: 'tanslateY(-10px)'})),
+  ])]
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('listAnimation', [
+      transition('* <=> *', [
+        query(':enter',
+          [style({
+            opacity: 0,
+            transform: 'translateY(-10px)',
+          }), stagger('200ms', animate('0.5s ease-out', style({opacity: 1, transform: 'translateY(0)'})))],
+          {optional: true},
+        ),
+        query(':leave',
+          animate('200ms', style({opacity: 0})),
+          {optional: true},
+        ),
+      ]),
+    ]),
+  ],
 })
 export class HomeComponent implements AfterViewInit {
 
